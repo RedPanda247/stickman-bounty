e86cc73fc0c5ba9d2dbfe082927b8fe192fd8bef
william solander <william.strandqvist@gmail.com>
2025-09-16 21:23:11 +0200

    added bevy, bevy app, and added empty being spawned

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use bevy::{
+    prelude::*,
+};
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, startup)
+        .run();
+}
+fn startup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Sprite {
+            color: Color::WHITE,
+            custom_size: Some(vec2(100., 100.)),
+            ..default()
+        },
+    ));
 }
