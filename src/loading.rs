1f773ed74dd5c67c40753f599dfad634af29a31f
William Solander Strandqvist <william.solanderstr@learnet.se>
2025-10-01 10:46:24 +0200

    major progress on A system for loading game states, Added individual loading screen info resources, improved load game content event reciever, loadable game states can be .into() to turn into GameState, check if loading complete function added, const min loading screen time added, system for spawning loading screen entities made

--- src/loading.rs
+++ src/loading.rs
@@ -1,49 +1,79 @@
 use crate::game_data::*;
-use bevy::prelude::*;
+use bevy::{prelude::*};
 
 pub struct LoadingPlugin;
 
+const MIN_LOADING_SCREEN_TIME_SECS: f32 = 4.0;
+
 impl Plugin for LoadingPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, load_level.run_if(not(in_state(GameState::Loading))))
-            .add_systems(OnEnter(GameState::Loading), load_game_content);
+        app.add_systems(Update, ev_load_game_content.run_if(not(in_state(GameState::Loading))))
+            .add_systems(OnEnter(GameState::Loading), (despawn_game_entities, spawn_loading_screen_entities))
+            .add_systems(Update, check_if_loading_complete.run_if(in_state(GameState::Loading)));
     }
 }
 
+// Resources to keep track of loading info
+
 #[derive(Resource)]
-struct PreviousGameState(LoadableGameStates);
+struct GameStateBeingLoaded(LoadableGameStates);
+
+#[derive(Resource)]
+struct AssetsBeingLoaded(Vec<UntypedHandle>);
+
+#[derive(Resource)]
+struct LoadingScreenStartTime(f32);
+
+// Events to trigger loading
 
 #[derive(Event)]
-pub struct LoadGameContent {
-    content_type: LoadableGameStates,
+pub struct LoadGameState {
+    game_state_to_load: LoadableGameStates,
+    loading_screen: LoadingScreen,
 }
 
 fn ev_load_game_content(
-    mut ev_load_game_content: EventReader<LoadGameContent>,
+    mut ev_load_game_content: EventReader<LoadGameState>,
     mut game_state: ResMut<NextState<GameState>>,
-    mut game_state_being_loaded: LoadableGameStates,
+    mut res_game_state_being_loaded: ResMut<GameStateBeingLoaded>,
+    mut res_loading_screen: ResMut<LoadingScreen>,
+    mut res_loading_screen_start_time: ResMut<LoadingScreenStartTime>,
+    time: Res<Time>,
 ) {
-
+    for event in ev_load_game_content.read() {
+        game_state.set(event.game_state_to_load.clone().into());
+        res_game_state_being_loaded.0 = event.game_state_to_load.clone();
+        *res_loading_screen = event.loading_screen.clone();
+        res_loading_screen_start_time.0 = time.elapsed_secs();
+    }
 }
 
+#[derive(Resource, Clone, Copy)]
 enum LoadingScreen {
-    Level(LevelIdentifier),
-    MainMenu,
+    FromGameState,
+    StartGame,
 }
 
-#[derive(Resource)]
-struct LoadingInfo {
-    content_being_loaded: LoadableGameStates,
-    previous_game_state: LoadableGameStates,
-    overite_loading_screen: Option<LoadingScreen>,
-    assets_loading: Vec<UntypedHandle>,
-}
-
+#[derive(Clone)]
 enum LoadableGameStates {
     Level(LevelIdentifier),
     MainMenu,
 }
 
+impl Into<GameState> for LoadableGameStates {
+    fn into(self) -> GameState {
+        match self {
+            LoadableGameStates::Level(_) => {
+                GameState::PlayingLevel
+            },
+            LoadableGameStates::MainMenu => {
+                GameState::MainMenu
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
 enum LevelIdentifier {
     Id(u8),
 }
@@ -51,21 +81,49 @@ enum LevelIdentifier {
 #[derive(Component)]
 struct LoadingScreenEntity;
 
-#[derive(Event)]
-pub struct LoadLevel {
-    level: LevelIdentifier,
-}
-
-fn load_game_content() {}
-
-pub fn load_level(mut ev_load_level: EventReader<LoadLevel>, mut game_state: ResMut<NextState<GameState>>) {
-    for load_level in ev_load_level.read() {
-        game_state.set(GameState::Loading);
-        // set loading info
+fn check_if_loading_complete(
+    mut commands: Commands,
+    mut game_state: ResMut<NextState<GameState>>,
+    mut res_assets_being_loaded: ResMut<AssetsBeingLoaded>,
+    qy_loading_screen_entities: Query<Entity, With<LoadingScreenEntity>>,
+    time: Res<Time>,
+    res_loading_screen_start_time: Res<LoadingScreenStartTime>,
+) {
+    let loading_screen_time_elapsed = time.elapsed_secs() - res_loading_screen_start_time.0;
+    if res_assets_being_loaded.0.is_empty() && loading_screen_time_elapsed >= MIN_LOADING_SCREEN_TIME_SECS {
+        for loading_screen_entity in qy_loading_screen_entities.iter() {
+            commands.entity(loading_screen_entity).despawn();
+        }
+        game_state.set(GameState::PlayingLevel);
     }
 }
 
-fn spawn_loading_screen_entities(mut command: Commands, loading_screen: LoadingScreen) {}
+fn spawn_loading_screen_entities(mut command: Commands, loading_screen: Res<LoadingScreen>, game_state_being_loaded: Res<GameStateBeingLoaded>) {
+    match *loading_screen {
+        LoadingScreen::FromGameState => {
+            match &game_state_being_loaded.0 {
+                LoadableGameStates::Level(level_identifier) => {
+                    match level_identifier {
+                        // TODO: Add more level loading screens as needed
+                        LevelIdentifier::Id(_) => {
+                            // spawn level loading screen for level 1
+                        }
+                    }
+                },
+                LoadableGameStates::MainMenu => {
+                    // spawn main menu loading screen
+                }
+            }
+        },
+        LoadingScreen::StartGame => {
+            // spawn start game loading screen
+        }
+    }
+    command.spawn((
+        LoadingScreenEntity,
+    ));
+
+}
 
 fn despawn_game_entities(mut command: Commands, qy_game_entities: Query<Entity, With<GameEntity>>) {
     for game_entity in qy_game_entities {
