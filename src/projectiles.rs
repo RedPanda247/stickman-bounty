4b04803b4d999d0e6b22b4b8c79731c06bb24f52
RedPanda247 <strandqvist.william@gmail.com>
2025-11-09 21:44:59 +0100

    bullet prejectile

--- src/projectiles.rs
+++ src/projectiles.rs
@@ -1,9 +1,19 @@
+use crate::game_data::*;
 use bevy::prelude::*;
 
 pub struct ProjectilesPlugin;
 
 impl Plugin for ProjectilesPlugin {
-    fn build(&self, app: &mut App) {
-        
-    }
+    fn build(&self, app: &mut App) {}
+}
+
+#[derive(Component)]
+struct Projectile;
+
+fn spawn_projectile(commands: &mut Commands, position: Vec3) {
+    commands.spawn((
+        GameEntity::LevelEntity, 
+        Projectile,
+        Transform::from_translation(position),
+    ));
 }
